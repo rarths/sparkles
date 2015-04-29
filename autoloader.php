936bf7120d5c2524031cd999f4bebb8cadc8f84e
rarths <robin@rarths.net>
2015-04-29 21:15:32 +0200

    Remove commented code

--- autoloader.php
+++ autoloader.php
@@ -13,7 +13,6 @@
  */
 spl_autoload_register(function ($class) {
     // project-specific namespace prefix
-    //$prefix = 'Foo\\Bar\\';
     $prefix = 'Rarths\\';
     // base directory for the namespace prefix
     $base_dir = __DIR__ . '/src/';
